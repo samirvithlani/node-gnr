0e41823c79741d3b2cfa711572a21a943dead04f
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-06 14:33:18 +0530

    node initilize

---
+++ student.js
@@ -0,0 +1,6 @@
+console.log("student file loaded")
+const studentName = "raj"
+const studentAge = 20
+// module.exports = studentName
+// module.exports = studentAge
+module.exports = {studentName, studentAge}
