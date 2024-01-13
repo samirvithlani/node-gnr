20572c06082e6b3e8a1a1ad5634276950282fd02
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-13 13:58:57 +0530

    user by id

--- app.js
+++ app.js
@@ -39,8 +39,51 @@ app.get("/users", (req, res) => {
 
 })
 
+app.get("/user1",(req,res)=>{
+
+    res.status(200).json({
+        data:users,
+        message:"Users list fetched successfully",
+    })
+})
+
+//:id is a dynamic parameter / wildcard parameter
+// app.get("/user/:id/:name",(req,res)=>{
 
 
+//     const id = req.params.id
+//     console.log(id)
+
+//     console.log(req.params)
+//     res.status(200).json({
+//         message:"api called....."
+//     })
+// })
+
+
+app.get("/user/:id",(req,res)=>{
+
+    const user = users.find((u)=>req.params.id == u.id)
+    if(user!=undefined || user!=null){
+        res.status(200).json({
+            data:user,
+            message:"User fetched successfully",
+        })
+    }
+    else{
+        res.status(404).json({
+            message:"User not found",
+        })
+    }
+        
+
+    // res.status(200).json({
+    //     data:user,
+    //     message:"User fetched successfully",
+    // })
+
+
+})
 
 const PORT = 3000;
 
