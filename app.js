b6c62746f23c92a7ac04b60b1d8b906baf16f503
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-13 13:36:46 +0530

    get request

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-
-
 
 //server creation using express...
 
-const PORT= 3000
+// app.get("/user", (req, res) => {
+//   console.log("user  api called...");
+//   //res.send("USER API CALLED...")
+//   res.json({
+//     name: "Raj",
+//     age: 21,
+//   });
+// });
 
-app.listen(PORT,()=>{
-    console.log("server started on port "+ PORT)
+var users = [
+    {
+        id:1,
+        name:"Raj",
+        age:21
+    },
+    {
+        id:2,
+        name:"Rahul",
+        age:22
+    },
+    {
+        id:3,
+        name:"Ramesh",
+        age:23
+    }
+]
+
+app.get("/users", (req, res) => {
+
+    res.json({
+        data:users,
+        message:"Users list fetched successfully",
+    })
+
 })
+
+
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("server started on port " + PORT);
+});
